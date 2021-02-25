662354432ead2daf575b2a915ccb5959b8c8f2e6
Harvey2504 <atibsamal96@gmail.com>
2021-02-25 12:21:06 +0530

    failing to learn

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,36 +13,57 @@ pipeline{
     stages{
         stage('maven-clean'){
             steps{
+                script{
+                    last_started=env.STAGE_NAME
+                }
                 bat 'mvn clean'
             }
         }
          stage('maven-validate'){
             steps{
+                script{
+                    last_started=env.STAGE_NAME
+                }
                 bat 'mvn validate'
             }
         }
          stage('maven-compile'){
             steps{
+                script{
+                    last_started=env.STAGE_NAME
+                }
                 bat 'mvn compile'
             }
         }
         stage('maven-test'){
             steps{
-                bat 'mvn test'
+                script{
+                    last_started=env.STAGE_NAME
+                }
+                sh 'mvn test'
             }
         }
         stage('maven-package'){
             steps{
+                script{
+                    last_started=env.STAGE_NAME
+                }
                 bat 'mvn package'
             }
         }
         stage('maven-install'){
             steps{
+                script{
+                    last_started=env.STAGE_NAME
+                }
                 bat 'mvn install'
             }
         }
         stage('sonar-analysis'){
             steps{
+                script{
+                    last_started=env.STAGE_NAME
+                }
                 withSonarQubeEnv('sonarqube4'){
                     bat 'mvn sonar:sonar'
                 }
@@ -50,12 +71,17 @@ pipeline{
         }
         stage('collect-artifacts'){
             steps{
+                script{
+                    last_started=env.STAGE_NAME
+                }
                 archiveArtifacts artifacts: 'target/*.jar', followSymlinks: false
             }
         }
-
         stage ('deploy to artifactory'){
             steps{
+                script{
+                    last_started=env.STAGE_NAME
+                }
                 rtUpload(
                     serverId: 'artifactoryserver',
                     spec: '''{
@@ -71,5 +97,13 @@ pipeline{
                 )
             }
         }
+        post{
+            failure{
+                mail bcc: '', body: "<b>Failure</b><br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br>Stage Name: $last_started <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "CI Pipeline Info: Project name -> ${env.JOB_NAME}", to: "samalati96@gmail.com";
+            }
+        }
+
+
+
     }
 }
